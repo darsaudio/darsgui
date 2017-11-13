bdfb82d4cb4f7b17387cfa45275a056c85725f3c
shaowei <wang.shaowei@bunnyedu.com>
2017-11-13 09:44:16 +0800

    auto load plugins

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include "mainwindow.h"
+#include <stdlib.h>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    int ret = 0;
+
+    ret = system("pacmd list-modules | grep module-dbus-protocol 1>/dev/null 2>&1");
+    if (ret) {
+        system("pacmd load-module module-dbus-protocol 1>/dev/null 2>&1");
+    }
+
+    ret = system("pacmd list-modules | grep module-dars-sink 1>/dev/null 2>&1");
+    if (ret) {
+        system("pacmd load-module module-dars-sink 1>/dev/null 2>&1");
+    }
+
     w.show();
 
     return a.exec();
